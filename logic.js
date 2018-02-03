4d0369bdf8b164f750ad231d480a414af3244ce7
Nicholas Brown <nicholasbrown@nicholass-mbp.uconnect.utah.edu>
2018-02-03 12:15:19 -0700

    fixed undefined linespacing issue and started solution to lines no tbeing replaced with letter. Also started the reset function.

--- logic.js
+++ logic.js
@@ -3,8 +3,9 @@
 // variables
 
 var randomWord;
+var randomWordChosen = [];
 var lineWord = [];
-var finalWord;
+// var finalWord;
 var compGuessWord = document.getElementById("compGuessWord");
 var guessesLeft = document.getElementById("guessesLeft");
 
@@ -18,68 +19,84 @@ var gameOver = document.getElementById("gameOver");
 var sciWords = ["hypothesis", "molecule", "mineral", "science", "astronomy", "evolution", "climate", "experiment"];
 
 
+function randomWordToLine() {
+    
+    // picks a random word from the dictionary array
+    randomWord = sciWords[Math.floor(Math.random() * sciWords.length)];
+
+    for (i=0; i < randomWord.length; i++) {
+        randomWordChosen[i] = randomWord.charAt(i);
+        console.log(randomWordChosen);
+    }
+    
+    // pushes underscore to the lineword array based on the length of the random word chosen
+    for (i = 0; i < randomWord.length; i++) {
+
+        lineWord[i] = ("_ ");
+        console.log(lineWord);
+    }
+    
+    /*getting undefined printed at beginning of lines
+    
+    adds each individual line from the lineWord array to the final word variable 
+    so it can print to the html*/
+
+    compGuessWord.textContent = lineWord.join(' ');
+    
+    
+    
+    // adds the number of user guesses that are left to the html
+    guessesLeft.textContent = userGuessScore = 7;
+}
+
+function reset () {
+ userGuesses.length = 0;
+ lineWord.length = 0;
+
+ userGuesses.length = 0;
+ gameOver.length = 0;
+}
+
+function userPlay() {
+    document.onkeyup = function () {
+        
+        // prints to the html "game over" if the player has 0 guesses left
+        if (userGuessScore === 0) {
+            guessesLeft.textContent = userGuessScore--;
+            gameOver.textContent = "GAME OVER!";
+            document.onkeyup = function () {
+            reset();
+            randomWordToLine();
+
+            }
+        }
+        
+        // "should" replace the line at "_" with the corresponding letter at the position in ramdomWord var
+        else if (randomWordChosen.includes(event.key)) {
+            lineWord.splice[randomWordChosen.charAt(event.key), 1, event.key]
+        }
+        
+        // alerts user if they have already picked a letter
+        else if (userGuesses.includes(event.key)) {
+            alert("You already chose that letter!");
+        }
+        
+        /* adds the letter to userGuesses id and remove one life if user choses wrong*/
+        else {
+            userGuesses.push(event.key);
+            userWords.textContent = userGuesses;
+            guessesLeft.textContent = userGuessScore--;
+            
+        }
+    }
+}
+
 document.onkeyup = function gameStart() {
     randomWordToLine();
     userPlay();
     console.log(randomWord);
     console.log(randomWord.length);
 }
-
-
-function randomWordToLine() {
-
-    // picks a random word from the dictionary array
-    randomWord = sciWords[Math.floor(Math.random() * sciWords.length)];
-
-    // pushes underscore to the lineword array based on the length of the random word chosen
-    for (i = 0; i < randomWord.length; i++) {
-        lineWord.push("_ ");
-    }
-
-    /*getting undefined printed at beginning of lines
-    
-    adds each individual line from the lineWord array to the final word variable 
-    so it can print to the html*/
-    for (i = 0; i < lineWord.length; i++) {
-        finalWord += lineWord[i];
-    }
-
-    // adds the lined version of the random word to the html
-    compGuessWord.textContent = finalWord;
-
-    // adds the number of user guesses that are left to the html
-    guessesLeft.textContent = userGuessScore = 7;
-}
-
-function userPlay() {
-    document.onkeyup = function () {
-
-        // prints to the html "game over" if the player has 0 guesses left
-        if (userGuessScore === 0) {
-            guessesLeft.textContent = userGuessScore--;
-            gameOver.textContent = "GAME OVER! Please refresh the browswer to play again."
-        }
-
-        // "should" replace the line at "_" with the corresponding letter at the position in ramdomWord var
-        else if (randomWord.includes(event.key)) {
-            lineWord.splice[randomWord.charAt(event.key), 1, event.key]
-        }
-
-        // alerts user if they have already picked a letter
-        else if (userGuesses.includes(event.key)) {
-            alert("You already chose that letter!");
-        }
-
-        /* adds the letter to userGuesses id and remove one life if user choses wrong*/
-        else {
-            userGuesses.push(event.key);
-            userWords.textContent = userGuesses;
-            guessesLeft.textContent = userGuessScore--;
-
-        }
-    }
-}
-
 
 
 
